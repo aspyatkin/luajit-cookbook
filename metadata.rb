ac4e3847e2c2401e64f436583f9675b9ab41900f
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2015-11-18 10:48:29 +0400

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,9 @@
 name 'luajit'
+maintainer 'Alexander Pyatkin'
+maintainer_email 'aspyatkin@gmail.com'
+license 'MIT'
 description 'Installs and configures luajit'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 
 recipe 'luajit', 'Installs and configures luajit'
